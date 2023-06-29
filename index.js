39415111e4a2042ed80f14be6c0d218a2b3d2205
Essence1987 <hwmelander@gmail.com>
2023-06-28 21:37:34 -0600

    created README file for project. Created Classes for Shapes and updated Test File.

--- index.js
+++ index.js
@@ -3,42 +3,44 @@ const fs = require('fs');
 const shapes = require('./lib/shapes');
 
 function createLogo() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            name: 'text',
-            message: 'Enter up to three characters:',
-        },
-        {
-            type: 'input',
-            name: 'textColor',
-            message: 'Enter the text color (keyword or hexadecimal):',
-        },
-        {
-            type: 'list',
-            name: 'shapeChoice',
-            message: 'Choose a shape:',
-            choices: Object.keys(shapes),
-        },
-        {
-            type: 'input',
-            name: 'shapeColor',
-            message: 'Enter the shape color (keyword or hexadecimal):',
-        },
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter up to three characters:',
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter the text color (keyword or hexadecimal):',
+      },
+      {
+        type: 'list',
+        name: 'shapeChoice',
+        message: 'Choose a shape:',
+        choices: Object.keys(shapes),
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (keyword or hexadecimal):',
+      },
     ])
     .then((answers) => {
-        console.log(answers);
-        // Create SVG content based on user input
-        const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
-        ${shapes[answers.shapeChoice](answers.shapeColor)}
+      console.log(answers);
+      // Create SVG content based on user input
+      const shape = new shapes[answers.shapeChoice](answers.shapeColor);
+      const svgContent = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+        ${shape.getSvgCode()}
         <text x="150" y="100" text-anchor="middle" alignment-baseline="middle" fill="${answers.textColor}">${answers.text}</text>
       </svg>`;
-    //   Save SVG to file
-    fs.writeFile('logo.svg', svgContent, (err) => {
+      // Save SVG to file
+      fs.writeFile('logo.svg', svgContent, (err) => {
         if (err) throw err;
         console.log('Generated logo.svg');
+      });
     });
-    });
-};
+}
 
 createLogo();
